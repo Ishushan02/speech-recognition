e882324be680ee3b90b2c42e34345ee9769f09ff
ishushan02 <ishananand2002@gmail.com>
2024-09-30 12:18:03 -0700

    Recording Audio from System

--- audio_operations.py
+++ audio_operations.py
@@ -51,8 +51,54 @@ signal_array = np.frombuffer(signal_wave, dtype=np.int16)
 times = np.linspace(0, t_audio, num =sample_freq )
 
 print(times.shape, signal_array.shape)
-times_truncated = signal_array[:len(times)]
+times_truncated = signal_array[:len(times)] # truncating such that the length matches and it shows in the graph
 
 plt.plot(times_truncated)
 plt.show()
+
+'''
+
+Recording audio from laptop using Python
+
+'''
+
+import pyaudio
+import wave
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+p = pyaudio.PyAudio()
+ 
+# starts recording
+stream = p.open(
+   format=FORMAT,
+   channels=CHANNELS,
+   rate=RATE,
+   input=True,
+   frames_per_buffer=FRAMES_PER_BUFFER
+)
+
+print("start recording...")
+
+frames = []
+seconds = 5
+for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+
+print("recording stopped")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+
+wf = wave.open("output.wav", 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
 
